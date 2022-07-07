a51bc8d7311b8ff83088c76d58db710c63869056
MathiasAbreu <mathias.trajano@ccc.ufcg.edu.br>
2022-07-07 10:43:13 -0300

    initial commit

---
+++ src/main/java/br/com/ufcg/easymocktests/EasymocktestsApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufcg.easymocktests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymocktestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymocktestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ufcg/easymocktests/EasymocktestsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufcg.easymocktests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymocktestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
