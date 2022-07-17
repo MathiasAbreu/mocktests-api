530faff6655e763ec303695d380742effe3f0421
Mathias Abreu <mathias.trajano@ccc.ufcg.edu.br>
2022-07-16 22:34:41 -0300

    fix in spring.factories file

--- lib/src/main/java/config/MockTestsConfiguration.java
+++ lib/src/main/java/config/MockTestsConfiguration.java
@@ -2,10 +2,8 @@ package config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "mocktests_library")
-@PropertySource("classpath:lib/src/main/config")
 public class MockTestsConfiguration {
 }
