4a8aab6bd26cde36081d9f6bb7b697237903c9e0
Mathias Abreu <mathias.trajano@ccc.ufcg.edu.br>
2022-07-17 13:49:36 -0300

    change in MockTestsConfiguration.class

--- lib/src/main/java/config/MockTestsConfiguration.java
+++ lib/src/main/java/config/MockTestsConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "mocktests_library")
+@ComponentScan(basePackages = "lib")
 public class MockTestsConfiguration {
 }
