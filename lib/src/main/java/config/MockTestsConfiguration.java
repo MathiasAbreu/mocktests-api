e5ef394ff1a3e945a88436c26bfffc4b004a6aa4
Mathias Abreu <mathias.trajano@ccc.ufcg.edu.br>
2022-07-16 22:18:35 -0300

    add PropertySource annotation in MockTestsConfiguration

--- lib/src/main/java/config/MockTestsConfiguration.java
+++ lib/src/main/java/config/MockTestsConfiguration.java
@@ -2,8 +2,10 @@ package config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "mocktests_library")
+@PropertySource("classpath:lib/src/main/config")
 public class MockTestsConfiguration {
 }
